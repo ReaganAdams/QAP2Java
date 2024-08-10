5712bfd19bdf460facfe4b37f2710e9f31365f46
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 00:46:02 -0230

    Person.java

---
+++ Person.java
@@ -0,0 +1,17 @@
+// Defines a person by name and address
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Address home;
+
+    public Person(String firstName, String lastName, Address home) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.home = home;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", " + home.toString();
+    }
+}
